9e1feeb585f4c5a53563e501b8338ac4bf49b4d1
vradioti <vasileios.radiotis@nokia.com>
2019-09-27 17:51:24 +0300

    Started implementation of PriorityQueue

---
+++ include/PriorityQueue.h
@@ -0,0 +1,37 @@
+#ifndef PRIORITYQUEUE_H
+#define PRIORITYQUEUE_H
+#include <vector>
+
+typedef struct m_node node;
+
+class PriorityQueue
+{
+
+    private:
+
+
+        node* m_head;
+
+    public:
+        PriorityQueue();
+        virtual ~PriorityQueue();
+
+        typedef struct m_node
+        {
+            short priority;
+            short vertex;
+            m_node *next;
+            m_node *previous;
+        }node;
+
+        void chgPriority(node* node, short priority);
+        void insertNode(node*);
+        node* minPriority();
+        node* top();
+        bool contains(node*);
+        short sizeOfQueue();
+
+
+};
+
+#endif // PRIORITYQUEUE_H
---
+++ src/PriorityQueue.cpp
@@ -0,0 +1,30 @@
+#include "PriorityQueue.h"
+
+PriorityQueue::PriorityQueue()
+{
+    //ctor
+}
+
+PriorityQueue::~PriorityQueue()
+{
+    //dtor
+}
+
+void PriorityQueue::chgPriority(node* Node, short priority)
+{
+    Node->priority = priority;
+    (Node->previous)->next = Node->next;
+    (Node->next)->previous = Node->previous;
+    Node->next = Node->previous = nullptr;
+
+
+    for( node* iterator_node = top(); iterator_node->next != nullptr; iterator_node->next = (iterator_node->next)->next)
+    {
+
+    }
+}
+
+node* PriorityQueue::top()
+{
+    return m_head;
+}
