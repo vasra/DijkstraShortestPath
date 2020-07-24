1705ea5cb80b8f4174da67a5dbf6768df3fcfc48
Vasileios Radiotis <livebythecreed37@gmail.com>
2020-07-24 18:59:11 +0300

    Continuing the implementation of the priority queue.

--- include/PriorityQueue.h
+++ include/PriorityQueue.h
@@ -1,23 +1,43 @@
 #ifndef PRIORITYQUEUE_H
 #define PRIORITYQUEUE_H
-#include <list>
 #include "Vertex.h"
 #include "Graph.h"
 
 class PriorityQueue
 {
     private:
-        struct node {
-            node(std::shared_ptr<Vertex>& vertex) {
-                std::shared_ptr<Vertex> v = vertex;
-                unsigned int weight = INT_MAX;
+        struct node
+        {
+            std::shared_ptr<Vertex> vertex;
+            unsigned int weight;
+
+            node(std::shared_ptr<Vertex>& v, int w = INT_MAX)
+            {
+                std::shared_ptr<Vertex> vertex = v;
+                unsigned int weight = w;
             }
+            bool operator<(node& n)
+            {
+                return weight < n.weight;
+            }
+         
         };
-        std::list<std::unique_ptr<node>> nodes;
+        std::vector<std::shared_ptr<node>> nodes;
+
     public:
         PriorityQueue(Graph& g);
         virtual ~PriorityQueue();
 
+        std::shared_ptr<node>& top();
+        int left(int i);
+        int right(int i);
+        int parent(int i);
+        int min_child(int i);
+        void insert(std::shared_ptr<node>& ptr);
+        void delete_min();
+        void percolate_up(int i);
+        void percolate_down(int i);
+        void swap(int a, int b);
 };
 
 #endif // PRIORITYQUEUE_H
--- src/PriorityQueue.cpp
+++ src/PriorityQueue.cpp
@@ -3,10 +3,69 @@
 PriorityQueue::PriorityQueue(Graph& g)
 {
     for( auto& vertex : g.getVertices() )
-        nodes.emplace_front(std::make_unique<node>(vertex));
+        nodes.emplace_back(std::make_shared<node>(vertex));
 }
 
 PriorityQueue::~PriorityQueue()
 {
     //dtor
+}
+
+std::shared_ptr<PriorityQueue::node>& PriorityQueue::top()
+{
+    return nodes.front();
+}
+
+int PriorityQueue::left(int i)
+{
+    return (2 * i + 1);
+}
+
+int PriorityQueue::right(int i)
+{
+    return (2 * i + 2);
+}
+
+int PriorityQueue::parent(int i)
+{
+    return (i - 1) / 2;
+}
+
+int PriorityQueue::min_child(int i)
+{
+    if (left(i) < nodes.size() && nodes[left(i)] < nodes[i])
+        return left(i);
+
+    if (right(i) < nodes.size() && nodes[right(i)] < nodes[i])
+        return i * 2;
+
+    return i * 2 + 1;
+}
+
+void PriorityQueue::insert(std::shared_ptr<node>& ptr)
+{
+    nodes.push_back(ptr);
+    percolate_up(nodes.size() - 1);
+}
+
+void PriorityQueue::delete_min()
+{
+    
+}
+
+void PriorityQueue::percolate_up(int i)
+{
+    if (i > 0 && nodes[i] < nodes[parent(i)])
+    {
+        swap(i, parent(i));
+        i = parent(i);
+        percolate_up(i);
+    }
+}
+
+void PriorityQueue::swap(int a, int b)
+{
+    std::shared_ptr<node> temp = nodes[a];
+    nodes[a] = nodes[b];
+    nodes[b] = temp;
 }
